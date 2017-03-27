960c72a7604e6eba709f4a6652830c527fec8053
marciosan <mrc.snts@gmail.com>
2017-03-27 10:12:33 +0100

    updated script to match the new directory tree

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/fish
 
 # dir containing outA.txt and others
-set OUTS_DIR src/ist/meic/pa/tests
+set OUTS_DIR tests
 
-for i in (find src/ist/meic/pa/tests/ | grep "Test" | grep -Eo '.\.' | grep -Eo '[^.]*' | sort)
-	java -jar keyConstructors.jar Test$i ^ /tmp/$i.out
+for i in (find tests/ | grep "Test" | grep -Eo '.\.' | grep -Eo '[^.]*' | sort)
+	java -cp 'keyConstructors.jar:bin/tests' ist.meic.pa.KeyConstructors Test$i ^ /tmp/$i.out
 	
 	echo -e \n\n $i =============================\n\n
 	diff /tmp/$i.out $OUTS_DIR/out$i.txt
