ea8b5b646fe5200a9f48a565fe2609cc4fdff9c2
marciosan <mrc.snts@gmail.com>
2017-03-27 17:07:23 +0100

    restored TestA to original file

--- tests/TestA.java
+++ tests/TestA.java
@@ -1,7 +1,9 @@
 public class TestA {
 	public static void main(String[] args) {
-		Widget wo = new Widget());
-		Widget w1 = new Widget("margin", 2);
-		Widget w2 = new Widget("width", 8, "height", 13, "margin", 21);
+		System.err.println(new Widget());
+		System.err.println(new Widget("width", 80));
+		System.err.println(new Widget("height", 30));
+		System.err.println(new Widget("margin", 2));
+		System.err.println(new Widget("width", 8, "height", 13, "margin", 21));
 	}
 }
